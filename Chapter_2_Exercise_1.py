4a1aefbaf54b2ef67a281fa3d103b6b3c2517bf5
jasfer <47032082+xtrnals@users.noreply.github.com>
2022-09-26 09:17:26 +0400

    Created using Colaboratory

---
+++ Chapter_2_Exercise_1.py
@@ -0,0 +1,57 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOSQM4KF7gbP4PgUI6wzKlU",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/xtrnals/Chapter-2/blob/main/Chapter_2_Exercise_1.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "YeA7K-KZNzgE",
+        "outputId": "0bc5ea5d-986a-4998-d178-43f84e9f6ffd"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Hello World!\n",
+            "Hello Earth!\n"
+          ]
+        }
+      ],
+      "source": [
+        "message = \"Hello World!\"\n",
+        "print (message)\n",
+        "message = \"Hello Earth!\"\n",
+        "print (message)"
+      ]
+    }
+  ]
+}
