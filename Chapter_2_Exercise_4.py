9d43510b943095a06467112eb88d91c13455aec7
jasfer <47032082+xtrnals@users.noreply.github.com>
2022-09-26 21:42:01 +0400

    Created using Colaboratory

---
+++ Chapter_2_Exercise_4.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPSvQHmjL1a08KPaNWuZgvk",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/xtrnals/Chapter-2/blob/main/Chapter_2_Exercise_4.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "JRHIaBf44r2c",
+        "outputId": "ca6cdce2-7cb7-42e6-e33d-57e998c23636"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "My favourite number is 17\n"
+          ]
+        }
+      ],
+      "source": [
+        "favenumber = 17\n",
+        "print (\"My favourite number is\", favenumber)"
+      ]
+    }
+  ]
+}
