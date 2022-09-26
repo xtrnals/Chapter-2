425a7f9db3cdc7fa6835cc3d4da0524e4c9a9997
jasfer <47032082+xtrnals@users.noreply.github.com>
2022-09-26 21:34:34 +0400

    Created using Colaboratory

---
+++ Chapter_2_Exercise_2.py
@@ -0,0 +1,53 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNkCfrgJhHfNcE4vjQA9JqM",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/xtrnals/Chapter-2/blob/main/Chapter_2_Exercise_2.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 3,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "3A0tw76_20If",
+        "outputId": "5d92b1a4-065a-4b35-aad3-f39246e38362"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Nelson Mandela once said, \"The greatest glory in living lies not in never falling, but in rising every time we fall.\"\n"
+          ]
+        }
+      ],
+      "source": [
+        "print (\"Nelson Mandela once said, \\\"The greatest glory in living lies not in never falling, but in rising every time we fall.\\\"\")"
+      ]
+    }
+  ]
+}
